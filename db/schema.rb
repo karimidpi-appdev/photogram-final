05ec9d599688aa8d052e79de8dd513d8bf95a1da
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-22 21:07:46 +0000

    Created comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_22_205752) do
+ActiveRecord::Schema.define(version: 2023_03_22_210448) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "author_id"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "fan_id"
