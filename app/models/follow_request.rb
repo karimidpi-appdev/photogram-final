61dbf40619276e04e80658635c6cbbb46bf12527
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-27 16:03:04 +0000

    Added validations and methods for follow request

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -9,5 +9,15 @@
 #  recipient_id :integer
 #  sender_id    :integer
 #
-class FollowRequest < ApplicationRecord
+class FollowRequest < 
+  validates(:recipient, {:presence => true })
+  validates(:sender, {:presence => true })
+  
+  def recipient
+    return User.where({ :id => self.recipient_id }).at(0)
+  end
+
+  def sender
+    return User.where({ :id => self.sender_id }).at(0)
+  end
 end
