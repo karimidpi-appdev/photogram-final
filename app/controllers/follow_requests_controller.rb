e41e8b4635eb0b9685ba98a0910fa8894283dcb8
Mohammad Ali Karimi <ali216k@gmail.com>
2023-04-05 06:04:40 +0000

    Follow request work for home page (users)

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -21,13 +21,28 @@ class FollowRequestsController < ApplicationController
     the_follow_request = FollowRequest.new
     the_follow_request.sender_id = params.fetch("query_sender_id")
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
-    the_follow_request.status = params.fetch("query_status")
+
+    recipient = User.where({ :id => the_follow_request.recipient_id }).first
+
+    if recipient.private == true
+      the_follow_request.status = "pending"
+    else
+      the_follow_request.status = "accepted"
+    end
+
+    # the_follow_request.status = params.fetch("query_status")
 
     if the_follow_request.valid?
       the_follow_request.save
-      redirect_to("/follow_requests", { :notice => "Follow request created successfully." })
+
+      if the_follow_request.status == "accepted"
+        redirect_to("/users/" + the_follow_request.recipient.username, { :notice => "Follow request created successfully." })
+      else
+        redirect_to("/users/", { :notice => "Follow request sent successfully." })
+
+      end
     else
-      redirect_to("/follow_requests", { :alert => the_follow_request.errors.full_messages.to_sentence })
+      redirect_to("/users", { :alert => the_follow_request.errors.full_messages.to_sentence })
     end
   end
 
--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -10,20 +10,20 @@
 #  sender_id    :integer
 #
 class FollowRequest < ApplicationRecord
+  validates(:recipient, { :presence => true })
 
-  validates(:recipient, {:presence => true })
+  validates(:sender, { :presence => true })
 
-  validates(:sender, {:presence => true })
+  # belongs_to(:sender, { :class_name => "User", :counter_cache => :sent_follow_requests_count })
 
+  # belongs_to(:recipient, { :class_name => "User", :counter_cache => :received_follow_requests_count })
 
-  # Direct Associations
+  belongs_to :recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id" }
 
-  belongs_to(:sender, { :class_name => "User", :counter_cache => :sent_follow_requests_count })
+  belongs_to :sender, { :required => true, :class_name => "User", :foreign_key => "sender_id" }
 
-  belongs_to(:recipient, { :class_name => "User", :counter_cache => :received_follow_requests_count })
+  has_many :following_photos, :through => :recipient, :source => :photos
 
-  
-  
   def recipient
     return User.where({ :id => self.recipient_id }).at(0)
   end
